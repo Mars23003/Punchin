8ff26e650dd43cac71db861037f8efc468aeebb4
MarsLin <54692980+Mars23003@users.noreply.github.com>
2020-03-13 17:18:16 +0800

    2

--- registrationT.php
+++ registrationT.php
@@ -10,12 +10,13 @@
     $stmt->execute();
     $count = $stmt->rowCount();//查詢到的行數
    // echo $count;
-   if(!$count=0){
+   if($count!=0){
     echo "已註冊";
     $sql = "INSERT INTO `regT`(`name`, `pwd`, `identify`, `email`, `phone`) VALUES ('$name','$pwd','$identity','$email','$phone');";
     $conn->exec($sql);
    }else{
-     echo "名稱重複了！！重新註冊！";
+     echo "名稱重複了！！重新註冊！ 三秒後回到註冊頁面";
+     header("Refresh: 3; url=registrationT.html");
    };
 
 $conn = null;
